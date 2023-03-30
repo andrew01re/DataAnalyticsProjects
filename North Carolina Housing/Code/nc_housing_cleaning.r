2269fc5953c9fd7cb6d621bc612c59884ffd7137
andrew01re <51674393+andrew01re@users.noreply.github.com>
2023-03-30 13:27:22 -0400

    Neatened code
    Added comments, moved code chunks around for better readability

--- North Carolina Housing/Code/nc_housing_cleaning.r
+++ North Carolina Housing/Code/nc_housing_cleaning.r
@@ -1,167 +1,167 @@
-install.packages("tidyverse")
-install.packages("skimr")
-install.packages("janitor")
-install.packages("sqldf")
+#Data Cleaning of tables created in SQL 
 
-library(tidyverse)
-library(skimr)
-library(janitor)
-library(sqldf)
+### Settubg up Environment
+  install.packages("tidyverse")
+  install.packages("skimr")
+  install.packages("janitor")
+  install.packages("sqldf")
 
-edu <- read_csv("county_edu.csv")
-demo <- read_csv("county_demographics.csv")
-emp <- read_csv("county_employment.csv")
-housing <- read_csv("county_housing.csv")
-inc <- read_csv("county_income.csv")
-commute <- read_csv("county_commute.csv")
-pop <- read_csv("county_population.csv")
-zill <- read_csv("zillow.csv")
+  library(tidyverse)
+  library(skimr)
+  library(janitor)
+  library(sqldf)
 
+### Importing data
+  edu <- read_csv("county_edu.csv")
+  demo <- read_csv("county_demographics.csv")
+  emp <- read_csv("county_employment.csv")
+  housing <- read_csv("county_housing.csv")
+  inc <- read_csv("county_income.csv")
+  commute <- read_csv("county_commute.csv")
+  pop <- read_csv("county_population.csv")
+  zill <- read_csv("zillow.csv")
 
+#### Education
 
-# Education #########################################
-head(edu)
-str(edu)
+  head(edu)
+  str(edu)
 
-# The county name is formatted Name\nCounty
-#There is at least one instance of the county name being >1 word like first\nsecond\nCounty
+  # The county name is formatted Name\nCounty
+  #There is at least one instance of the county name being >1 word like first\nsecond\nCounty
 
-edu$county <- gsub("[\n]"," ", edu$county)    #replaces all new lines with spaces
-
-
-#reformat avg_SAT_score to an int
-edu$avg_SAT_score <- gsub(",","",edu$avg_SAT_score) %>% 
-  as.numeric()
-
-# Demographics ######################################
-head(demo)
-str(demo)
-
-demo$county <- gsub("[\n]"," ", demo$county)   #replaces all new lines with spaces
-
-colnames(demo) <- c("county", "0-19", "20-34", "35-44", "45-54", "55-64", "65+") 
-
-#pivot table to long format  
-demo = tidyr::pivot_longer(data = demo,
-                                cols = colnames(demo)[-1],
-                                names_to = 'age_range',
-                                values_to = 'percent_of_pop',
-                                values_drop_na = FALSE)
+  edu$county <- gsub("[\n]"," ", edu$county)    #replaces all new lines with spaces
 
 
-# Employment ########################################
+  #reformat avg_SAT_score to an int
+  edu$avg_SAT_score <- gsub(",","",edu$avg_SAT_score) %>% 
+    as.numeric()
 
-head(emp)
-str(emp)
+#### Demographics
+  head(demo)
+  str(demo)
+
+  demo$county <- gsub("[\n]"," ", demo$county)   #replaces all new lines with spaces
+
+  colnames(demo) <- c("county", "0-19", "20-34", "35-44", "45-54", "55-64", "65+") 
+
+  #pivot table to long format  
+  demo = tidyr::pivot_longer(data = demo,
+                                  cols = colnames(demo)[-1],
+                                  names_to = 'age_range',
+                                  values_to = 'percent_of_pop',
+                                  values_drop_na = FALSE)
 
 
-emp$county <- gsub("[\n]"," ", emp$county)   #replaces all new lines with spaces
+#### Employment
 
-emp <- emp %>% mutate_if(is.numeric,as.character)
-# remove the commas and $s from all columns (except first two) and convert the columns to numeric
-for (i in 3:length(emp)){
-  for (j in 1:nrow(emp[,i])) {
-      emp[j,i] <- gsub("[$,.]","",emp[j,i])
-  j=j+1 
+  head(emp)
+  str(emp)
+
+
+  emp$county <- gsub("[\n]"," ", emp$county)   #replaces all new lines with spaces
+
+  emp <- emp %>% mutate_if(is.numeric,as.character)
+  # remove the commas and $s from all columns (except first two) and convert the columns to numeric
+  for (i in 3:length(emp)){
+    for (j in 1:nrow(emp[,i])) {
+        emp[j,i] <- gsub("[$,.]","",emp[j,i])
+    j=j+1 
+    }
+    i=i+1
   }
-  i=i+1
-}
 
-emp[,-1] <- emp[,-1] %>% 
-  mutate_if(is.character, as.numeric)
+  emp[,-1] <- emp[,-1] %>% 
+    mutate_if(is.character, as.numeric)
 
 
-emp = tidyr::pivot_longer(data = emp,
-                                cols = colnames(emp)[-1:-2],
-                                names_to = 'industry',
-                                values_to = 'job_count',
-                                values_drop_na = FALSE)
+  emp = tidyr::pivot_longer(data = emp,
+                                  cols = colnames(emp)[-1:-2],
+                                  names_to = 'industry',
+                                  values_to = 'job_count',
+                                  values_drop_na = FALSE)
 
 
-# Housing ########################################
+#### Housing
+  #housing and zillow data will need to be combined into one table. Starting with housing.csv:
 
-head(housing)
-str(housing)
+  head(housing)
+  str(housing)
 
-housing$county <- gsub(", nc"," County", housing$county) %>%  # removes the suffix of the county
-  str_to_title()
+  #Fix county name format
+  housing$county <- gsub(", nc"," County", housing$county) %>%  # removes the suffix of the county
+    str_to_title()
 
-housing$date <- as.Date(paste0(as.character(housing$date), '01'), format='%Y%m%d')
+  #reformat date to date datatype. Datapoints are monthly, so added 01 as day
+  housing$date <- as.Date(paste0(as.character(housing$date), '01'), format='%Y%m%d')
 
-head(zill)
-str(zill)
+  head(zill)
+  str(zill)
 
-#pivot the data from wide to long
-long_zill = tidyr::pivot_longer(data = zill,
-                                cols = colnames(zill)[-1],
-                                names_to = 'date',
-                                values_to = 'median_home_price',
-                                values_drop_na = FALSE)
-###Since the data is monthly, and I want to combine with housing data, transform the date to the first of each month
-head(long_zill)
-substr(long_zill$date, 9,10) <- "01"
-long_zill$date <- as.Date(long_zill$date)
-housing_m <- merge(housing, long_zill)
+  #pivot the data from wide to long
+  long_zill = tidyr::pivot_longer(data = zill,
+                                  cols = colnames(zill)[-1],
+                                  names_to = 'date',
+                                  values_to = 'median_home_price',
+                                  values_drop_na = FALSE)
+  ###Since the data is monthly, and I want to combine with housing data, transform the date to the first of each month
+  head(long_zill)
+  substr(long_zill$date, 9,10) <- "01"
+  long_zill$date <- as.Date(long_zill$date)
+  housing_m <- merge(housing, long_zill)
+  
+  #This merge resulted in at least one NULL value. Need to investigate:
+ 
+  missing_counties <- sqldf('SELECT DISTINCT h.county FROM housing h EXCEPT SELECT DISTINCT m.county FROM housing_m m')
+  missing_counties #returned Mcdowell County as the only result
 
-head(housing_m)
-str(housing_m)
+  housing_counties <-sqldf('SELECT DISTINCT county FROM housing')
+  zillow_counties <-sqldf('SELECT DISTINCT county FROM long_zill')
+  setdiff(housing_counties, zillow_counties)
 
+  # The issue is that the d in McDowell is lowercase in the housing data frame.
+  housing$county <- gsub("Mcdowell","McDowell", housing$county)
+  print(unique(housing_m$county),na.print="NULL",n=110)
+#### Income
 
-lapply(housing_m,function(x) { length(which(is.na(housing_m)))})
+  head(inc)
+  str(inc)
 
-print(unique(housing_m$county),na.print="NULL",n=110)
+  inc$county <- gsub("[\n]"," ", inc$county)  #replaces all new lines with spaces
+                      
 
-missing_counties <- sqldf('SELECT DISTINCT h.county FROM housing h EXCEPT SELECT DISTINCT m.county FROM housing_m m')
-missing_counties #returned Mcdowell County as the only result
+#### Commute 
+  head(commute)
+  str(commute)
 
-housing_counties <-sqldf('SELECT DISTINCT county FROM housing')
-zillow_counties <-sqldf('SELECT DISTINCT county FROM long_zill')
-housing_counties
-zillow_counties
+  commute$county <- gsub("[\n]"," ", commute$county)   #replaces all new lines with spaces
 
-# The issue is that the d in McDowell is lowercase in the housing data frame.
-housing$county <- gsub("Mcdowell","McDowell", housing$county)
-############ Income
+  #convert to long format
+  commute = tidyr::pivot_longer(data = commute,
+                                  cols = colnames(commute)[-1:-2],
+                                  names_to = 'method',
+                                  values_to = 'percent_of_pop',
+                                  values_drop_na = FALSE)
+  commute$method <- gsub("method_bike_perc","Bike", commute$method) %>%
+    {gsub("method_pub_trans_perc","Public Transit", .)} %>%
+    {gsub("method_other_perc","Other", .)} %>%
+    {gsub("method_walked_perc","Walk", .)} %>%
+    {gsub("wfh_perc","Work From Home", .)} %>%
+    {gsub("method_vehicle_perc","Personal Vehicle", .)} 
 
-head(inc)
-str(inc)
+#### Population Predicitons
+  head(pop)
+  str(pop)
 
-inc$county <- gsub("[\n]"," ", inc$county)  #replaces all new lines with spaces
-                     
-
-# Commute ############################################################
-head(commute)
-str(commute)
-
-commute$county <- gsub("[\n]"," ", commute$county)   #replaces all new lines with spaces
-
-#convert to long format
-commute = tidyr::pivot_longer(data = commute,
-                                cols = colnames(commute)[-1:-2],
-                                names_to = 'method',
-                                values_to = 'percent_of_pop',
-                                values_drop_na = FALSE)
-commute$method <- gsub("method_bike_perc","Bike", commute$method) %>%
-  {gsub("method_pub_trans_perc","Public Transit", .)} %>%
-  {gsub("method_other_perc","Other", .)} %>%
-  {gsub("method_walked_perc","Walk", .)} %>%
-  {gsub("wfh_perc","Work From Home", .)} %>%
-  {gsub("method_vehicle_perc","Personal Vehicle", .)} 
-
-# Population Predicitons ########################################
-head(pop)
-str(pop)
-
-pop$county <- paste0(pop$county, " County")
-pop <- filter(pop, county != "State County")
+  pop$county <- paste0(pop$county, " County")
+  pop <- filter(pop, county != "State County")
 
 
-#separate lat and long from geom_center
-pop <- separate(data = pop, col = geom_center, into = c("lattitude", "longitude"), sep = ", ")
-pop$lattitude <-as.numeric(pop$lattitude)
-pop$longitude <-as.numeric(pop$longitude)
+  #separate lat and long from geom_center
+  pop <- separate(data = pop, col = geom_center, into = c("lattitude", "longitude"), sep = ", ")
+  pop$lattitude <-as.numeric(pop$lattitude)
+  pop$longitude <-as.numeric(pop$longitude)
 
-# Export to CSV ########################################
+# Export to CSV for upload to Tableau
 
 write.csv(edu, "education.csv", row.names=FALSE)
 write.csv(demo, "demographics.csv", row.names=FALSE)
--- North Carolina Housing/Code/sql_exploration_and_selection.sql
+++ North Carolina Housing/Code/sql_exploration_and_selection.sql
@@ -1,29 +1,13 @@
-------------------------------------------------------------------------
----------------------Data Selection / Reduction-------------------------
-------------------------------------------------------------------------
+--Before using SQL, the Original Data files were uploaded into a BigQuery Database
+--BigQuery was used to first explore the data and then create the initial table views that will be cleaned in RStudio before uploading to Tableau
 
---Commute as of 2019---------------------------------------------
-CREATE OR REPLACE VIEW nc_housing_views.county_commute AS
-  SELECT 
-    Distinct County_Name                                AS county,
-    _2019_Est_Avg_Travel_Time_To_Work__Minutes__ACS_    AS avg_commute_min,
-    ROUND(100*(_2019___Drove_Car_Truck_Van_Alone__ACS_ + _2019___Carpooled_Car_Truck_Van__ACS_),1) AS method_vehicle_perc,
-    ROUND(100*_2019___Working_from_Home__ACS_,1)        AS wfh_perc,
-    ROUND(100*_2019___Public_Transp__ACS_,1)            AS method_pub_trans_perc,
-    ROUND(100*_2019___Bicycle__ACS_,1)                  AS method_bike_perc,
-    ROUND(100*_2019___Walked__ACS_,1)                   AS method_walked_perc,
-    ROUND(100*_2019___Taxi__Motorcycle__Other__ACS_,1)  AS method_other_perc
-
-  FROM `portfolio-project-380614.nc_housing.NC_county_commute` 
-  WHERE County_Name IS NOT NULL
-
---Housing metrics-----------------------------------------------
+---------------------Housing metrics------------------------
+--These tables will be used to create the initial dashboard page that shows the median home price (zhvi zillow data) and other housing metrics
 CREATE OR REPLACE VIEW nc_housing_views.county_housing AS 
  SELECT
     core.county_name                           AS county,
     core.month_date_yyyymm                     AS date,
     core.active_listing_count,
-    core.median_listing_price,
     core.median_days_on_market,
     core.median_square_feet                    AS median_sqft,
     core.median_listing_price_per_square_foot  AS median_price_per_sqft
@@ -31,79 +15,6 @@ CREATE OR REPLACE VIEW nc_housing_views.county_housing AS
   WHERE core.county_name LIKE "%, nc"
   ORDER BY core.county_name, core.month_date_yyyymm
 
---Demographics----------------------------------------------------------
-CREATE OR REPLACE VIEW nc_housing_views.county_demographics AS
-  SELECT
-    Distinct County_Name AS county,
-      ROUND(100*_2019_Est___Population_0_19__ACS_,1) AS age_0_19_perc,
-    ROUND(100*(_2019_Est___Population_20_24__ACS_+_2019_Est___Population_25_34__ACS_),1) AS age_20_34_perc,
-    ROUND(100*_2019_Est___Population_35_44__ACS_,1) AS age_35_44_perc,
-    ROUND(100*_2019_Est___Population_45_54__ACS_,1) AS age_45_54_perc,
-    ROUND(100*_2019_Est___Population_55_64__ACS_,1) AS age_55_64,
-    ROUND(100*_2019_Est___Population_65___ACS_,1) AS age_65_up
-
-  FROM `portfolio-project-380614.nc_housing.NC_county_demograph`
-  ORDER BY County_Name
-
---Education---------------------------------------------------------
-CREATE OR REPLACE VIEW nc_housing_views.county_education AS
-  SELECT
-    Distinct County_Name AS county,
-    _2020_Average_SAT_score__1600_scale___NCDPI_              AS avg_SAT_score,
-    _2019_Est_Education_Attainment____High_School_Grad___ACS_ AS highest_achieved_hs,
-    _2019_Est_Education_Attainment_____Bachelors___ACS_       AS hgihest_achieved_bach
-  FROM `portfolio-project-380614.nc_housing.NC_county_edu` 
-
---Employment----------------------------------------------------
-CREATE OR REPLACE VIEW nc_housing_views.county_employment AS
-  SELECT
-    Distinct County_Name AS county,
-    ROUND(100*Mar2022_Prelim___Unemployment_Rate__LAUS_,1)      AS unemp_rate_mar_2020,
-    _2021_Employment_All_Industries__QCEW_ AS all_industries,
-    _2021_Employment_Agriculture_Forestry_Fishing_Hunting__QCEW_ AS agriculture_forestry_fishing_hunting,
-    _2021_Employment_Mining__QCEW_ AS mining,
-    _2021_Employment_Utilities__QCEW_ AS utilities,
-    _2021_Employment_Construction__QCEW_ AS construction,
-    _2021_Employment_Manufacturing__QCEW_ AS manufacturing,
-    _2021_Employment_Wholesale_Trade__QCEW_ AS wholesale_trade,
-    _2021_Employment_Retail_Trade__QCEW_ AS retail_trade,
-    _2021_Employment_Transportation_Warehousing__QCEW_ AS transportation_warehousing,
-    _2021_Employment_Information__QCEW_ AS information,
-    _2021_Employment_Finance_Insurance__QCEW_ AS finance_insurance,
-    _2021_Employment_Real_Estate_Rental_Leasing__QCEW_ AS real_estate_rental_leasing,
-    _2021_Employment_Professional_Technical_Services__QCEW_ AS professional_technical_services,
-    _2021_Employment_Mgt_of_Companies_Enterprises__QCEW_ AS mgt_of_companies_enterprises,
-    _2021_Employment_Administrative_Waste_Services__QCEW_ AS administrative_waste_services,
-    _2021_Employment_Educational_Services__QCEW_ AS educational_services,
-    _2021_Employment_Health_Care_Social_Assistance__QCEW_ AS health_care_social_assistance,
-    _2021_Employment_Arts_Entertain_Recreation__QCEW_ AS arts_entertain_recreation,
-    _2021_Employment_Accommodation_Food_Services__QCEW_ AS accommodation_food_services,
-    _2021_Employment_Other_Services_Ex__Public_Admin__QCEW_ AS other_services_ex__public_admin,
-    _2021_Employment_Public_Admin__QCEW_ AS public_admin,
-    _2021_Employment_Unclassified__QCEW_ AS unclassified
-  FROM `portfolio-project-380614.nc_housing.NC_county_employment`
-  ORDER BY County_Name
-
-
---Income-------------------------------------------------------------
-CREATE OR REPLACE VIEW nc_housing_views.county_income AS
-SELECT
-  Distinct County_Name AS county,
-  _2019_Est_Median_Worker_Earnings__ACS_                          AS median_worker_inc_2019,
-  _2020_Median_Household_Income__SAIPE_                           AS median_hh_income_2020,
-  ROUND(100*_2020_Est___Pop__Income_Below_Poverty__SAIPE_,1)      AS pop_perc_below_poverty_202
- 
-FROM `portfolio-project-380614.nc_housing.NC_county_income`
-
---population growth---------------------------------------------------
-CREATE OR REPLACE VIEW nc_housing_views.county_pop_predictions AS
-SELECT
-      County as county,
-      Year as year,
-      Population as population
-    FROM `portfolio-project-380614.nc_housing.NC_county_pop_predictions`
-
---zillow-----------------------------------------------------
 CREATE OR REPLACE VIEW nc_housing_views.zillow AS
   SELECT
     RegionName AS county,
@@ -197,3 +108,92 @@ CREATE OR REPLACE VIEW nc_housing_views.zillow AS
   ORDER BY RegionName
 
 
+-------The rest of these tables will be used to create county profile pages showing demographic and lifestyle data for the selected county
+----------------------Commute as of 2019-------------------------
+CREATE OR REPLACE VIEW nc_housing_views.county_commute AS
+  SELECT 
+    Distinct County_Name                                AS county,
+    _2019_Est_Avg_Travel_Time_To_Work__Minutes__ACS_    AS avg_commute_min,
+    ROUND(100*(_2019___Drove_Car_Truck_Van_Alone__ACS_ + _2019___Carpooled_Car_Truck_Van__ACS_),1) AS method_vehicle_perc,
+    ROUND(100*_2019___Working_from_Home__ACS_,1)        AS wfh_perc,
+    ROUND(100*_2019___Public_Transp__ACS_,1)            AS method_pub_trans_perc,
+    ROUND(100*_2019___Bicycle__ACS_,1)                  AS method_bike_perc,
+    ROUND(100*_2019___Walked__ACS_,1)                   AS method_walked_perc,
+    ROUND(100*_2019___Taxi__Motorcycle__Other__ACS_,1)  AS method_other_perc
+
+  FROM `portfolio-project-380614.nc_housing.NC_county_commute` 
+  WHERE County_Name IS NOT NULL
+
+
+
+-------------------Demographics-----------------------------
+CREATE OR REPLACE VIEW nc_housing_views.county_demographics AS
+  SELECT
+    Distinct County_Name AS county,
+      ROUND(100*_2019_Est___Population_0_19__ACS_,1) AS age_0_19_perc,
+    ROUND(100*(_2019_Est___Population_20_24__ACS_+_2019_Est___Population_25_34__ACS_),1) AS age_20_34_perc,
+    ROUND(100*_2019_Est___Population_35_44__ACS_,1) AS age_35_44_perc,
+    ROUND(100*_2019_Est___Population_45_54__ACS_,1) AS age_45_54_perc,
+    ROUND(100*_2019_Est___Population_55_64__ACS_,1) AS age_55_64,
+    ROUND(100*_2019_Est___Population_65___ACS_,1) AS age_65_up
+
+  FROM `portfolio-project-380614.nc_housing.NC_county_demograph`
+  ORDER BY County_Name
+
+------------------------Education----------------------------
+CREATE OR REPLACE VIEW nc_housing_views.county_education AS
+  SELECT
+    Distinct County_Name AS county,
+    _2020_Average_SAT_score__1600_scale___NCDPI_              AS avg_SAT_score,
+    _2019_Est_Education_Attainment____High_School_Grad___ACS_ AS highest_achieved_hs,
+    _2019_Est_Education_Attainment_____Bachelors___ACS_       AS hgihest_achieved_bach
+  FROM `portfolio-project-380614.nc_housing.NC_county_edu` 
+
+------------------------Employment-----------------------------
+CREATE OR REPLACE VIEW nc_housing_views.county_employment AS
+  SELECT
+    Distinct County_Name AS county,
+    ROUND(100*Mar2022_Prelim___Unemployment_Rate__LAUS_,1)      AS unemp_rate_mar_2020,
+    _2021_Employment_All_Industries__QCEW_ AS all_industries,
+    _2021_Employment_Agriculture_Forestry_Fishing_Hunting__QCEW_ AS agriculture_forestry_fishing_hunting,
+    _2021_Employment_Mining__QCEW_ AS mining,
+    _2021_Employment_Utilities__QCEW_ AS utilities,
+    _2021_Employment_Construction__QCEW_ AS construction,
+    _2021_Employment_Manufacturing__QCEW_ AS manufacturing,
+    _2021_Employment_Wholesale_Trade__QCEW_ AS wholesale_trade,
+    _2021_Employment_Retail_Trade__QCEW_ AS retail_trade,
+    _2021_Employment_Transportation_Warehousing__QCEW_ AS transportation_warehousing,
+    _2021_Employment_Information__QCEW_ AS information,
+    _2021_Employment_Finance_Insurance__QCEW_ AS finance_insurance,
+    _2021_Employment_Real_Estate_Rental_Leasing__QCEW_ AS real_estate_rental_leasing,
+    _2021_Employment_Professional_Technical_Services__QCEW_ AS professional_technical_services,
+    _2021_Employment_Mgt_of_Companies_Enterprises__QCEW_ AS mgt_of_companies_enterprises,
+    _2021_Employment_Administrative_Waste_Services__QCEW_ AS administrative_waste_services,
+    _2021_Employment_Educational_Services__QCEW_ AS educational_services,
+    _2021_Employment_Health_Care_Social_Assistance__QCEW_ AS health_care_social_assistance,
+    _2021_Employment_Arts_Entertain_Recreation__QCEW_ AS arts_entertain_recreation,
+    _2021_Employment_Accommodation_Food_Services__QCEW_ AS accommodation_food_services,
+    _2021_Employment_Other_Services_Ex__Public_Admin__QCEW_ AS other_services_ex__public_admin,
+    _2021_Employment_Public_Admin__QCEW_ AS public_admin,
+    _2021_Employment_Unclassified__QCEW_ AS unclassified
+  FROM `portfolio-project-380614.nc_housing.NC_county_employment`
+  ORDER BY County_Name
+
+
+---------------------------Income----------------------------------
+CREATE OR REPLACE VIEW nc_housing_views.county_income AS
+SELECT
+  Distinct County_Name AS county,
+  _2019_Est_Median_Worker_Earnings__ACS_                          AS median_worker_inc_2019,
+  _2020_Median_Household_Income__SAIPE_                           AS median_hh_income_2020,
+  ROUND(100*_2020_Est___Pop__Income_Below_Poverty__SAIPE_,1)      AS pop_perc_below_poverty_202
+ 
+FROM `portfolio-project-380614.nc_housing.NC_county_income`
+
+----------------------Population Growth---------------------------
+CREATE OR REPLACE VIEW nc_housing_views.county_pop_predictions AS
+SELECT
+      County as county,
+      Year as year,
+      Population as population
+    FROM `portfolio-project-380614.nc_housing.NC_county_pop_predictions`
